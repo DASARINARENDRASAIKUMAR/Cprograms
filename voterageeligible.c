5547754958b88cdb17d560ee1afe60c5d0830bf9
DASARINARENDRASAIKUMAR <saikumardasari9346@gmail.com>
2024-10-21 12:10:02 +0530

    Create voterageeligible.c

---
+++ voterageeligible.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() {
+    int a;
+    int b=18;
+    
+    printf("enteryour age");
+    scanf("%d",&a);
+if (a>b)
+{
+    printf("he/her is eligible to vote");
+}
+else
+{
+    printf("he/her is  not eligible to vote");
+}
+}
